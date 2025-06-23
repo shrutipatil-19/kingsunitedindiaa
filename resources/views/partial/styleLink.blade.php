c3df6cc8f70a965a7e23e80125b4dd8fb4449aef
shrutipatil-19 <shrupatil1319@gmail.com>
2025-06-23 10:07:56 +0530

    remove

--- resources/views/partial/styleLink.blade.php
+++ resources/views/partial/styleLink.blade.php
@@ -17,4 +17,3 @@
     href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
     rel="stylesheet">
  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
- <link rel="stylesheet" href="">
