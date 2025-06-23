a4533b5148beb67de0b10a3bdad8c5c1600591ac
shrutipatil-19 <shrupatil1319@gmail.com>
2025-06-23 10:07:21 +0530

    style

--- resources/views/partial/styleLink.blade.php
+++ resources/views/partial/styleLink.blade.php
@@ -17,3 +17,4 @@
     href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
     rel="stylesheet">
  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="">
