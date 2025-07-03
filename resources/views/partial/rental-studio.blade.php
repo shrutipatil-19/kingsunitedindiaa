207383e4b65feb149ede1b483790e0bf47cdedd4
shrutipatil-19 <shrupatil1319@gmail.com>
2025-07-03 18:52:51 +0530

    home

--- resources/views/partial/rental-studio.blade.php
+++ resources/views/partial/rental-studio.blade.php
@@ -23,11 +23,8 @@
                   <div class="swiper-slide ms-trending3-item p-relative fix mb-40">
                       <div class="ms-trending3-img w-img ms-br-15 p-relative fix mb-30" data-aos="fade-up" data-aos-delay="100" data-aos-duration="800">
                           <a href="{{ route('studio.book.form', ['studio' => 'studio-1']) }}">
-                              <img src="{{ asset('assets/img/trending/01.jpg') }}" alt="Studio 1 Image">
+                              <img src="{{ asset('assets/img/studio/studio1.jpg') }}" alt="Studio 1 Image">
                           </a>
-                          <div class="trending__number">
-                              <span>01</span>
-                          </div>
                           <!-- availability -->
                           <div class="ms-availability-badge bg-success text-white p-1 rounded position-absolute top-0 end-0 m-2">
                               Available
@@ -45,75 +42,144 @@
                               <li class="list-inline-item"><i class="fa-solid fa-snowflake"></i> AC</li>
                               <li class="list-inline-item"><i class="fa-solid fa-mirror"></i> Mirrors</li>
                           </ul>
-                          <div class="d-flex justify-content-between align-items-center">
-                              <span class="fw-bold text-primary">₹500/hr</span>
-                              <a href="{{ route('studio.book.form', ['studio' => 'studio-1']) }}" class="btn btn-primary btn-sm">Book Now</a>
+                          <div class="d-flex justify-content-between align-items-start">
+                              <p class="fw-bold mb-1 text-white">
+                                  ₹1500/Hour
+                                  <!-- <del class="text-muted ms-2">₹1,00000</del> -->
+                              </p>
+
+                              <div class="trending-btn mb-40 d-flex justify-content-lg-end">
+                                  <a class="border__btn" href="{{ route('studio.book.form', ['studio' => 'studio-1']) }}">Book Now</a>
+                              </div>
                           </div>
                       </div>
                   </div>
-
+                  <!-- Slide 2 -->
                   <div class="swiper-slide ms-trending3-item p-relative fix mb-40">
                       <div class="ms-trending3-img w-img ms-br-15 p-relative fix mb-30" data-aos="fade-up" data-aos-delay="200" data-aos-duration="800">
-                          <a href="{{ route('studio.book.form', ['studio' => 'studio-2']) }}"><img src="{{ asset('assets/img/trending/01.jpg') }}"
-                                  alt="trending image"></a>
+                          <a href="{{ route('studio.book.form', ['studio' => 'studio-2']) }}">
+                              <img src="{{ asset('assets/img/studio/studio1.jpg') }}" alt="Studio 1 Image">
+                          </a>
 
-
-                          <div class="trending__number">
-                              <span>02</span>
-                          </div>
                       </div>
                       <div class="ms-trending3-content">
-                          <h3 class="ms-trending3-title mb-25"><a href="{{ route('studio.book.form', ['studio' => 'studio-2']) }}">STUDIO 2 - 750 SQ.FT</a></h3>
-                          <p class="ms-trending3-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt iste quis.
+                          <h3 class="ms-trending3-title mb-15">
+                              <a href="{{ route('studio.book.form', ['studio' => 'studio-1']) }}">STUDIO 2 - 750 SQ.FT</a>
+                          </h3>
+                          <p class="ms-trending3-text mb-10">
+                              Perfect for small dance groups and video shoots.
                           </p>
+                          <ul class="list-inline mb-15 small">
+                              <li class="list-inline-item"><i class="fa-solid fa-music"></i> Sound System</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-snowflake"></i> AC</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-mirror"></i> Mirrors</li>
+                          </ul>
+                          <div class="d-flex justify-content-between align-items-start">
+                              <p class="fw-bold mb-1 text-white">
+                                  ₹1500/Hour
+                                  <!-- <del class="text-muted ms-2">₹1,00000</del> -->
+                              </p>
+
+                              <div class="trending-btn mb-40 d-flex justify-content-lg-end">
+                                  <a class="border__btn" href="{{ route('studio.book.form', ['studio' => 'studio-2']) }}">Book Now</a>
+                              </div>
+                          </div>
                       </div>
                   </div>
+
                   <div class="swiper-slide ms-trending3-item p-relative fix mb-40">
                       <div class="ms-trending3-img w-img ms-br-15 p-relative fix mb-30" data-aos="fade-up" data-aos-delay="300" data-aos-duration="800">
-                          <a href="{{ route('studio.book.form', ['studio' => 'studio-3']) }}"><img src="{{ asset('assets/img/trending/01.jpg') }}"
-                                  alt="trending image"></a>
+                          <a href="{{ route('studio.book.form', ['studio' => 'studio-3']) }}">
+                              <img src="{{ asset('assets/img/studio/studio1.jpg') }}" alt="Studio 1 Image">
+                          </a>
 
-
-                          <div class="trending__number">
-                              <span>03</span>
-                          </div>
                       </div>
                       <div class="ms-trending3-content">
-                          <h3 class="ms-trending3-title mb-25"><a href="{{ route('studio.book.form', ['studio' => 'studio-3']) }}">STUDIO 3A- 650 SQ.FT</a></h3>
-                          <p class="ms-trending3-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum fugiat corrupti sint.
+                          <h3 class="ms-trending3-title mb-15">
+                              <a href="{{ route('studio.book.form', ['studio' => 'studio-3']) }}">STUDIO 3A- 650 SQ.FT</a>
+                          </h3>
+                          <p class="ms-trending3-text mb-10">
+                              Perfect for small dance groups and video shoots.
                           </p>
+                          <ul class="list-inline mb-15 small">
+                              <li class="list-inline-item"><i class="fa-solid fa-music"></i> Sound System</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-snowflake"></i> AC</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-mirror"></i> Mirrors</li>
+                          </ul>
+                          <div class="d-flex justify-content-between align-items-start">
+                              <p class="fw-bold mb-1 text-white">
+                                  ₹2,000/Hour
+                                  <!-- <del class="text-muted ms-2">₹1,00000</del> -->
+                              </p>
+
+                              <div class="trending-btn mb-40 d-flex justify-content-lg-end">
+                                  <a class="border__btn" href="{{ route('studio.book.form', ['studio' => 'studio-2']) }}">Book Now</a>
+                              </div>
+                          </div>
                       </div>
                   </div>
+
                   <div class="swiper-slide ms-trending3-item p-relative fix mb-40">
                       <div class="ms-trending3-img w-img ms-br-15 p-relative fix mb-30" data-aos="fade-up" data-aos-delay="400" data-aos-duration="800">
-                          <a href="{{ route('studio.book.form', ['studio' => 'studio-4']) }}"><img src="{{ asset('assets/img/trending/01.jpg') }}"
-                                  alt="trending image"></a>
+                          <a href="{{ route('studio.book.form', ['studio' => 'studio-3']) }}">
+                              <img src="{{ asset('assets/img/studio/studio1.jpg') }}" alt="Studio 1 Image">
+                          </a>
 
-
-                          <div class="trending__number">
-                              <span>04</span>
-                          </div>
                       </div>
                       <div class="ms-trending3-content">
-                          <h3 class="ms-trending3-title mb-25"><a href="{{ route('studio.book.form', ['studio' => 'studio-4']) }}">STUDIO 3B- 650 SQ.FT</a></h3>
-                          <p class="ms-trending3-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, harum eligendi! Dolorum!
+                          <h3 class="ms-trending3-title mb-15">
+                              <a href="{{ route('studio.book.form', ['studio' => 'studio-4']) }}">STUDIO 3B- 650 SQ.FT</a>
+                          </h3>
+                          <p class="ms-trending3-text mb-10">
+                              Perfect for small dance groups and video shoots.
                           </p>
+                          <ul class="list-inline mb-15 small">
+                              <li class="list-inline-item"><i class="fa-solid fa-music"></i> Sound System</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-snowflake"></i> AC</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-mirror"></i> Mirrors</li>
+                          </ul>
+                          <div class="d-flex justify-content-between align-items-start">
+                              <p class="fw-bold mb-1 text-white">
+                                  ₹2,000/Hour
+                                  <!-- <del class="text-muted ms-2">₹1,00000</del> -->
+                              </p>
+
+                              <div class="trending-btn mb-40 d-flex justify-content-lg-end">
+                                  <a class="border__btn" href="{{ route('studio.book.form', ['studio' => 'studio-4']) }}">Book Now</a>
+                              </div>
+                          </div>
                       </div>
                   </div>
+
                   <div class="swiper-slide ms-trending3-item p-relative fix mb-40">
                       <div class="ms-trending3-img w-img ms-br-15 p-relative fix mb-30" data-aos="fade-up" data-aos-delay="500" data-aos-duration="800">
-                          <a href="{{ route('studio.book.form', ['studio' => 'studio-5']) }}"><img src="{{ asset('assets/img/trending/01.jpg') }}"
-                                  alt="trending image"></a>
+                          <a href="{{ route('studio.book.form', ['studio' => 'studio-3']) }}">
+                              <img src="{{ asset('assets/img/studio/studio1.jpg') }}" alt="Studio 1 Image">
+                          </a>
 
-
-                          <div class="trending__number">
-                              <span>05</span>
-                          </div>
                       </div>
                       <div class="ms-trending3-content">
-                          <h3 class="ms-trending3-title mb-25"><a href="{{ route('studio.book.form', ['studio' => 'studio-5']) }}">STUDIO 3 FULL</a></h3>
-                          <p class="ms-trending3-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, harum eligendi! Dolorum!
+                          <h3 class="ms-trending3-title mb-15">
+                              <a href="{{ route('studio.book.form', ['studio' => 'studio-5']) }}">STUDIO 3 FULL</a>
+                          </h3>
+                          <p class="ms-trending3-text mb-10">
+                              Perfect for small dance groups and video shoots.
                           </p>
+                          <ul class="list-inline mb-15 small">
+                              <li class="list-inline-item"><i class="fa-solid fa-music"></i> Sound System</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-snowflake"></i> AC</li>
+                              <li class="list-inline-item"><i class="fa-solid fa-mirror"></i> Mirrors</li>
+                          </ul>
+                          <div class="d-flex justify-content-between align-items-start">
+                              <p class="fw-bold mb-1 text-white">
+                                  ₹4,000/Hour
+                                  <!-- <del class="text-muted ms-2">₹1,00000</del> -->
+                              </p>
+
+                              <div class="trending-btn mb-40 d-flex justify-content-lg-end">
+                                  <a class="border__btn" href="{{ route('studio.book.form', ['studio' => 'studio-5']) }}">Book Now</a>
+                              </div>
+                          </div>
                       </div>
                   </div>
               </div>
@@ -126,3 +192,14 @@
       </div>
   </section>
   <!-- Trending area start -->
+
+  @push('style')
+
+  <style>
+      .ms-trending3-content {
+          padding-inline-end: 15px;
+          padding-inline-start: 15px;
+      }
+  </style>
+
+  @endpush
