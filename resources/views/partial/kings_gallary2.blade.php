eaa4c6840f4cccd1e83ba81ca1129c9133135b14
shrutipatil-19 <shrupatil1319@gmail.com>
2025-07-04 13:18:55 +0530

    kings

---
+++ resources/views/partial/kings_gallary2.blade.php
@@ -0,0 +1,254 @@
+<div id="" class="ms-partner-area fix pb-60 pt-60">
+
+    <main class="main container">
+        <div class="row justify-content-center bdFadeUp">
+            <div class="col-xl-6">
+                <div class="section__title-wrapper text-center mb-65 bd-title-anim">
+                    <h2 class="section__title"><span class="text-bg">Kings Crew </span>
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="gallery">
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/1.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/2.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="200" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Automobile</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/3.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="300" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/4.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="400" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/5.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="500" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Bicycle</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/6.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="600" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Office</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/7.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="700" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Office</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/8.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="800" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Cityscape</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/9.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="900" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Cityscape</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/10.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/11.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/12.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/13.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/14.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/14.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+                <figure>
+                    <img src="{{ asset('assets/img/kings crew/14.jpg') }}" alt="the kings" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="800">
+                    <!-- <figcaption>
+                        <h3>Mountains</h3>
+                        <p>the kings consectetur adipisicing elit.</p>
+                    </figcaption> -->
+                </figure>
+
+
+            </div>
+        </div>
+    </main>
+</div>
+<!-- Partner Area End Here  -->
+
+@push('style')
+
+<style>
+    img {
+        max-inline-size: 100%;
+    }
+
+    .logo {
+        display: flex;
+        justify-content: center;
+        font-size: 3rem;
+        line-height: 1.5;
+    }
+
+    .main {
+        grid-column: main;
+        padding: 1.25rem;
+    }
+
+    /* Photo Gallery */
+    .gallery {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(12.5rem, 1fr));
+        grid-auto-rows: 12.5rem;
+        grid-auto-flow: dense;
+        gap: 0.75rem;
+    }
+
+    .gallery figure {
+        /* container-type: inline-size; */
+        container: figure / inline-size;
+        overflow: hidden;
+
+        display: grid;
+        /* grid-template-columns: 1fr; */
+        /* grid-template-rows: 1fr; */
+
+        /*
+    Because 1fr = minmax(auto, 1fr),
+    to prevent the grid blowout,
+    set the minimum to zero.
+  */
+        grid-template-columns: minmax(0, 1fr);
+        grid-template-rows: minmax(0, 1fr);
+    }
+
+    .gallery img {
+        inline-size: 100%;
+        block-size: 100%;
+        object-fit: cover;
+
+        /* grid-row: 1 / -1; */
+        /* grid-column: 1 / -1; */
+        grid-area: 1 / 1 / -1 / -1;
+
+        transition: scale 1s ease-in-out;
+    }
+
+    .gallery figure:hover img {
+        scale: 1.125;
+    }
+
+    .gallery figcaption {
+        grid-area: 1 / 1 / -1 / -1;
+        align-self: end;
+        z-index: 1;
+
+        padding: 3em 15% 1em 1em;
+        color: #fff;
+        font-size: 0.875rem;
+        line-height: 1.3;
+        background: linear-gradient(transparent 25%, rgb(0 0 0 / 0.75));
+        display: none;
+    }
+
+    @container figure (width > 20rem) {
+        .gallery figcaption {
+            display: block;
+        }
+    }
+
+    .gallery figcaption h3 {
+        font-size: 1.0625rem;
+    }
+
+    .gallery figcaption p {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+
+    @media (width > 600px) {
+        .gallery figure:nth-child(1) {
+            grid-area: span 2 / span 2;
+        }
+
+        .gallery figure:nth-child(4n + 1) {
+            grid-row: span 2;
+        }
+
+        .gallery figure:nth-child(4n + 2) {
+            grid-column: span 2;
+        }
+    }
+</style>
+
+@endpush
