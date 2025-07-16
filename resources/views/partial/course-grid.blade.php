837dd43097503724d7b07e8c79abea07c42623cc
Kunal Mahajan <kunalmahajan976757@gmail.com>
2025-07-16 15:43:04 +0530

    home page change

--- resources/views/partial/course-grid.blade.php
+++ resources/views/partial/course-grid.blade.php
@@ -1,5 +1,23 @@
+ 
+ <style>
+    .ele3 {
+            position: absolute;
+                top: 16%;
+                right: 0%;
+                opacity: 0.6 !important;
+                transform: rotateY(180deg) !important;
+             }
+     
+             .ele3 img {
+                 width: calc(40% - 340px);
+                 height: 100%;
+                 object-fit: cover;
+                 z-index: -1;
+             }
+    </style>
+
  <!-- courses Area Start -->
- <section class="ms-news-area ms-bg-2 pt-130 pb-90 ">
+ <section class="ms-news-area ms-bg-2 pt-130 pb-90 p-relative">
      <div class="container">
          <div class="row align-items-end mb-25 bdFadeUp">
              <div class="col-lg-6">
@@ -12,8 +30,11 @@
          </div>
          <!-- Swiper -->
          <div class="row">
+            <div class="ele3 aos-init aos-animate" data-aos="fade-right" data-aos-duration="2000" bis_skin_checked="1">
+                <img src="{{ asset('assets/img/course/Layer5.png') }}" alt="">
+            </div>
              <!-- <div class="col-lg-6 pt-lg-4"> -->
-             <div class="col-lg-6 mt-lg-4 mt-3">
+             <div class="col-lg-6 mt-lg-4 mt-3 align-content-center">
                  <div class="achievement-card d-flex flex-lg-row flex-column">
                      <div class="img-box position-relative bg-black2 align-content-center custome-radius-top">
                          <img src="{{ asset('assets/img/course/diplomaStreet.jpeg') }}" alt="Diploma in Street Style" class="w-100 h-100 object-cover custome-radius-top">
@@ -46,7 +67,7 @@
              </div>
 
              <!-- Slide 2 -->
-             <div class="col-lg-6 mt-lg-4 mt-3">
+             <div class="col-lg-6 mt-lg-4 mt-3 align-content-center">
                  <div class="achievement-card d-flex flex-lg-row flex-column">
                      <div class="img-box bg-black2 align-content-center custome-radius-top">
                          <img src="{{ asset('assets/img/course/course5.jpg') }}" alt="choreo lab" class="w-100 h-100 object-cover custome-radius-top">
@@ -72,7 +93,7 @@
              </div>
 
              <!-- Slide 3 -->
-             <div class=" col-lg-6 mt-lg-5 mt-3">
+             <div class=" col-lg-6 mt-lg-5 mt-3 align-content-center">
                  <div class="achievement-card d-flex flex-lg-row flex-column">
                      <div class="img-box position-relative bg-black2 align-content-center custome-radius-top">
                          <img src="{{ asset('assets/img/course/dream2dream.jpeg') }}" alt="dream 2 dance" class="w-100 h-100 object-cover custome-radius-top">
@@ -103,7 +124,7 @@
              </div>
 
              <!-- Slide 4 -->
-             <div class="col-lg-6 mt-lg-5 mt-3">
+             <div class="col-lg-6 mt-lg-5 mt-3 align-content-center">
                  <!-- <div class="achievement-card d-flex flex-row " data-aos="fade-up" data-aos-delay="400" data-aos-duration="800"> -->
                  <div class="achievement-card d-flex flex-lg-row flex-column">
                      <div class="img-box bg-black2 align-content-center custome-radius-top">
@@ -136,7 +157,7 @@
         background-color: #0e0e0e;
     }
      .achievement-card {
-         height: auto;
+         height: 85%;
          box-shadow: 0 4px 12px rgba(222, 64, 56, 0.4);
          border: 1px solid #de4038;
      }
